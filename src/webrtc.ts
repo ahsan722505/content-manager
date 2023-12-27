373e305dfb43c364744fd52ea204d87f50f34624
ahsan722505 <ahsantahir722505@gmail.com>
2023-12-27 23:44:52 +0500

    adds webrtc code

---
+++ src/webrtc.ts
@@ -0,0 +1,87 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = import.meta.env.RENDERER_VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.RENDERER_VITE_SUPABASE_KEY;
+
+const client = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const channel = client.channel("signaling-server");
+
+channel
+  .on("broadcast", { event: "signal" }, (payload) => {
+    if (payload.offer) {
+      // Handle incoming offer
+      handleOffer(payload.offer);
+    } else if (payload.ice_candidate) {
+      // Handle incoming ICE candidate
+      handleIceCandidate(payload.ice_candidate);
+    } else if (payload.answer) {
+      // Handle incoming answer
+      handleAnswer(payload.answer);
+    }
+  })
+  .subscribe();
+
+const configuration = {
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};
+
+const peerConnection = new RTCPeerConnection(configuration);
+
+const dataChannel = peerConnection.createDataChannel("clipboard");
+
+dataChannel.addEventListener("message", (event) => {
+  console.log(event.data);
+});
+
+// Set up offer/answer negotiation
+async function createOffer() {
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  // Send the offer to the other peer through Supabase
+  channel.send({
+    type: "broadcast",
+    event: "signal",
+    payload: { offer: peerConnection.localDescription },
+  });
+}
+
+createOffer();
+
+peerConnection.onicecandidate = (event) => {
+  if (event.candidate) {
+    // Send the ICE candidate to the other peer through Supabase
+    channel.send({
+      type: "broadcast",
+      event: "signal",
+      payload: { ice_candidate: event.candidate },
+    });
+  }
+};
+
+// Function to handle incoming offer
+async function handleOffer(offer: RTCSessionDescriptionInit) {
+  await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+  // Create an answer
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+  // Send the answer to the other peer through Supabase
+  channel.send({
+    type: "broadcast",
+    event: "signal",
+    payload: { answer: peerConnection.localDescription },
+  });
+}
+
+// Function to handle incoming ICE candidate
+async function handleIceCandidate(
+  iceCandidate: RTCIceCandidateInit | undefined
+) {
+  await peerConnection.addIceCandidate(new RTCIceCandidate(iceCandidate));
+}
+
+async function handleAnswer(answer: RTCSessionDescriptionInit) {
+  await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+}
